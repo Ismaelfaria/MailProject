592c9ed63f7b64c9ff65d397e7e01bdfb091f880
limaismael8901 <limaismael8901@gmail.com>
2024-07-22 20:00:27 -0300

    Criei o projeto e add 2 referencias (junit e javax.mail).

---
+++ enviando-mail/src/test/java/enviando/mail/AppTest.java
@@ -0,0 +1,14 @@
+package enviando.mail;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@org.junit.Test
+    public void testMail() {
+    	
+    }
+}
